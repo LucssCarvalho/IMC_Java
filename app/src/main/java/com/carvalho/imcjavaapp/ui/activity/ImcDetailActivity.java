b5cdbf34b8f5b06991fdf168641ec17db2b0ec2c
Lucas Carvalho <49245496+LucssCarvalho@users.noreply.github.com>
2021-07-20 18:35:15 -0300

    update imc detail activity

--- app/src/main/java/com/carvalho/imcjavaapp/ui/activity/ImcDetailActivity.java
+++ app/src/main/java/com/carvalho/imcjavaapp/ui/activity/ImcDetailActivity.java
@@ -1,16 +1,31 @@
 package com.carvalho.imcjavaapp.ui.activity;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 import com.carvalho.imcjavaapp.R;
+import com.carvalho.imcjavaapp.model.imcRecord;
 
 public class ImcDetailActivity extends AppCompatActivity {
+
+    private TextView viewResult;
+    private TextView viewInfoResult;
+    private com.carvalho.imcjavaapp.model.imcRecord imcRecord;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_imc_detail);
+        viewResult = findViewById(R.id.textView_imcResult);
+        viewInfoResult = findViewById(R.id.textView_imcInfoResult);
+        final Intent data = getIntent();
+        if (data.hasExtra("imc")) {
+            imcRecord = (imcRecord) data.getSerializableExtra("imc");
+            viewResult.setText(imcRecord.imc);
+            viewInfoResult.setText(imcRecord.infoImc);
+        }
     }
 }
